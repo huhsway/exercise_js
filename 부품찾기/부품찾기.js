e2c02cddda28d0ad82fa58b09430acaea0b72d71
huhsway <seunghoe9542@naver.com>
2022-10-08 18:21:53 +0900

    코딩 테스트 연습 추가

---
+++ 부품찾기/부품찾기.js
@@ -0,0 +1,21 @@
+let [n, input, m, target] = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
+
+function solution(n, input, m, target) {
+
+    const mySet = new Set();
+
+    for (let i = 0; i < n; i++){
+        mySet.add(input[i]);
+    }
+
+    for (let i = 0; i < m; i++) {
+        if (mySet.has(target[i])){
+            console.log('yes');
+        }
+        else {
+            console.log('no');
+        }
+    }
+}
+
+solution(n,input,m,target)
