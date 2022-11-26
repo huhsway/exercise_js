296673e1406dd5a9e93e0a22906122cac63181fa
huhsway <seunghoe9542@naver.com>
2022-11-26 18:11:06 +0900

    빗물 추가, 슬라이딩 윈도우 수정

--- 빗물/빗물.js
+++ 빗물/빗물.js
@@ -1,1 +1,34 @@
 // https://www.acmicpc.net/problem/14719
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname+'/input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [n, m] = input.shift().split(' ').map(Number);
+const arr = input.shift().split(' ').map(Number);
+
+function solution(n, m, arr) {
+
+    let left = 0, right = arr.length - 1;
+    let leftMax = arr[left], rightMax = arr[right];
+    let result = 0;
+
+    while (left < right) {
+        leftMax = Math.max(leftMax, arr[left]);
+        rightMax = Math.max(rightMax, arr[right]);
+
+        if (leftMax <= rightMax) {
+            result += leftMax - arr[left];
+            left += 1;
+        }
+        
+        else {
+            result += (rightMax - arr[right]);
+            right -= 1;
+        }
+    }
+
+    return result;
+}
+
+console.log(solution(n,m,arr))
--- 슬라이딩윈도우/슬라이딩윈도우.js
+++ 슬라이딩윈도우/슬라이딩윈도우.js
@@ -18,8 +18,4 @@ function solution(n, k, arr) {
     return result;
   }
   
-  const n = 10;
-  const k = 3;
-  const arr = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-  
-  console.log(solution(n, k, arr));
+console.log(solution(10, 3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]));
