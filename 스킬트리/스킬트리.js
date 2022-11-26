67d28a03127e02de382a7c5e257b249d976144ee
허승회 <35622185+huhsway@users.noreply.github.com>
2022-11-27 01:44:12 +0900

    Update 스킬트리.js
    search함수 dfs로 변경

--- 스킬트리/스킬트리.js
+++ 스킬트리/스킬트리.js
@@ -1,14 +1,14 @@
 // 스킬 트리 문제
 // 출처 : http://pgrblues.egloos.com/m/5317734#n
 
-function search(skillTree, costTree, rootIndex, skillPoint) {
+function dfs(skillTree, costTree, rootIndex, skillPoint) {
     let sumSp = 0
     if (skillTree[rootIndex].length === 0) {
         return costTree[rootIndex] = skillPoint
     }
 
     for (const childIdx of skillTree[rootIndex]) {
-        sumSp += search(skillTree, costTree, childIdx, skillPoint)
+        sumSp += dfs(skillTree, costTree, childIdx, skillPoint)
     }
     return costTree[rootIndex] = sumSp
 }
@@ -36,7 +36,7 @@ function solution(total_sp, skills) {
     const sp = Math.floor(total_sp / (max + adjCnt))
 
     const costTree = Array(max + 1)
-    search(skillTree, costTree, 1, sp)
+    dfs(skillTree, costTree, 1, sp)
 
     for (let i = 0; i < costTree.length; i++) {
         answer.push(costTree[i]);
