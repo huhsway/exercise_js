ebbf4cc17b95dd561055821f323cdd9714c20ba8
huhsway <seunghoe9542@naver.com>
2023-01-30 11:23:58 +0900

    직관적이게 변경

--- LeetCode/Dynamic Programming/Word_Break.js
+++ LeetCode/Dynamic Programming/Word_Break.js
@@ -13,8 +13,10 @@ var wordBreak = function (s, wordDict) {
         for (let j = 0; j < wordDict.length; j++) {
             const word = wordDict[j];
             const word_len = word.length;
-            if (dp[i - word_len] && s.slice(i - word_len, i) === word) {
-                dp[i] = true;
+            if (i - word_len >= 0) {
+                if (dp[i - word_len] && s.slice(i - word_len, i) === word) {
+                    dp[i] = true;
+                }
             }
         }
     }
