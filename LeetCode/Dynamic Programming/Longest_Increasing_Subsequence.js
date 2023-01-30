45a5b396c772a9204a81f50d54410241b22da63f
huhsway <seunghoe9542@naver.com>
2023-01-30 11:03:18 +0900

    문제목록, 문제 수정

--- LeetCode/Dynamic Programming/Longest_Increasing_Subsequence.js
+++ LeetCode/Dynamic Programming/Longest_Increasing_Subsequence.js
@@ -2,18 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function (nums) {
-    const dp = Array(nums.length).fill(1);
-    let ans = 1;
+var lengthOfLIS = function(nums) {
+    
+    const dp = Array(nums.length + 1).fill(1);
+    let maxValue = 1;
 
-    for (let i = 1; i < nums.length; i++) {
-        let max = 0;
-        for (let j = i - 1; j >= 0; j--) {
-            if (nums[j] < nums[i]) max = Math.max(max, dp[j]);
+    for (let i = 1; i <= nums.length; i++) {
+        for (let j = i-1; j >= 0; j--) {
+            if (nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+                maxValue = Math.max(maxValue, dp[i])
+            }
         }
-        dp[i] = max + 1;
-        ans = Math.max(ans, dp[i]);
     }
 
-    return ans;
+    return maxValue
 };
