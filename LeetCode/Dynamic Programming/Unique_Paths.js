572b064161582f5b3525f0c4fa828f6a521d5e73
huhsway <seunghoe9542@naver.com>
2023-01-30 16:32:38 +0900

    uniquePaths 풀이 변경

--- LeetCode/Dynamic Programming/Unique_Paths.js
+++ LeetCode/Dynamic Programming/Unique_Paths.js
@@ -4,20 +4,13 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-    const result = [[]];
-    // Adding the first row of ’1’s to the array
-    for (let i = 0; i < n; i += 1) {
-        result[0].push(1);
-    }
-    // iterating over each of the rows
-    for (let i = 1; i < m; i += 1) {
-        result.push([1]);    // adding 1 to the first, left most square
-
-        // Getting the total for the current square
-        for (let j = 1; j < n; j += 1) {
-            result[i][j] = result[i][j - 1] + result[i - 1][j];
-        }
-    }
-    // Return the bottom right hand value that has the total.
-    return result[m - 1][n - 1];
-};
+    const dp = Array(m).fill().map(() => Array(n).fill(1));
+      
+      for (let i = 1; i < m; i++) {
+          for (let j = 1; j < n; j++) {
+              dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+          }
+      }
+      
+      return dp[m - 1][n - 1];
+  };
