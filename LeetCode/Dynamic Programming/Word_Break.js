bf1e14e28115a0cc5c6fa9d4959530369a1f26e4
huhsway <seunghoe9542@naver.com>
2023-01-30 15:57:27 +0900

    dp 코드 수정

--- LeetCode/Dynamic Programming/Word_Break.js
+++ LeetCode/Dynamic Programming/Word_Break.js
@@ -3,23 +3,24 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function (s, wordDict) {
-    const dp = [true];
-    for (let i = 0; i < s.length; i++) {
-        dp.push(false);
-    }
+var wordBreak = function(s, wordDict) {
+    
+    const dp = Array(s.length+1).fill(false);
 
-    for (let i = 0; i < s.length + 1; i++) {
+    dp[0] = true;
+
+    for (let i = 0; i <= s.length; i++) {
         for (let j = 0; j < wordDict.length; j++) {
             const word = wordDict[j];
-            const word_len = word.length;
-            if (i - word_len >= 0) {
-                if (dp[i - word_len] && s.slice(i - word_len, i) === word) {
+            const wordLength = word.length;
+            if (i - wordLength >= 0) {
+                if (dp[i-wordLength] && s.slice(i-wordLength, i) === word){
                     dp[i] = true;
+                    continue
                 }
             }
         }
     }
 
-    return dp[dp.length - 1];
+    return dp[s.length]
 };
