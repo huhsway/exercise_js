5bcdcc780613ce9c810eea071124b274832ccdcb
huhsway <seunghoe9542@naver.com>
2023-02-16 16:49:20 +0900

    선택정렬 swap 수정

--- 자료구조/정렬/선택정렬.js
+++ 자료구조/정렬/선택정렬.js
@@ -4,10 +4,12 @@ const selectionSort = (arr) => {
         for(let j = i + 1 ; j < arr.length; j++){
             if(arr[j] < arr[min]) min = j;
         }
-        [arr[i], arr[min]] = [arr[min], arr[i]];
+        let temp = arr[min]
+        arr[min] = arr[i];
+        arr[i] = temp;
     }
     return arr;
 }
 
-selectionSort([2, 4, 33, 17, 8, 45, 1, 7, 10, 37]);
+console.log(selectionSort([2, 4, 33, 17, 8, 45, 1, 7, 10, 37]));
 // [1, 2, 4, 7, 8, 10, 17, 33, 37, 45]
