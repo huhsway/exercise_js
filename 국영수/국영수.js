8fa4393a8314a777372d68eeba28fa60cee2983d
허승회 <35622185+huhsway@users.noreply.github.com>
2023-12-28 11:01:55 +0900

    Update 국영수.js

--- 국영수/국영수.js
+++ 국영수/국영수.js
@@ -2,34 +2,34 @@
 
 let [n, ...arr] = require('fs').readFileSync(__dirname+'/input.txt').toString().trim().split('\n');
 const names = [];
-result = arr
-        .map(v => v.split(" ").map(vv => Number(vv)||vv)) //Number(vv)||vv는 문자는 문자로 냅두고 문자형 숫자는 숫자로 바꾸는 거
-        .sort((a, b) => {
-            if (a[1] < b[1]) return 1
-            else if (a[1] > b[1]) return -1
-            else {
-                if (a[2] > b[2]) return 1
-                else if (a[2] < b[2]) return -1
-                else {
-                    if (a[3] < b[3]) return 1
-                    else if (a[3] > b[3]) return -1
-                    else {
-                        if (a[0] > b[0]) return 1
-                        else if (a[0] < b[0]) return -1
-                        else return 0
-                    }
-                }
-            }
-        });
-
 // result = arr
-//     .map(v => v.split(" ").map(vv => Number(vv) || vv))
-//     .sort((a, b) => 
-//         b[1] - a[1] || 
-//         a[2] - b[2] || 
-//         b[3] - a[3] || 
-//         a[0].toString().localeCompare(b[0].toString())
-//     );
+//         .map(v => v.split(" ").map(vv => Number(vv)||vv)) //Number(vv)||vv는 문자는 문자로 냅두고 문자형 숫자는 숫자로 바꾸는 거
+//         .sort((a, b) => {
+//             if (a[1] < b[1]) return 1
+//             else if (a[1] > b[1]) return -1
+//             else {
+//                 if (a[2] > b[2]) return 1
+//                 else if (a[2] < b[2]) return -1
+//                 else {
+//                     if (a[3] < b[3]) return 1
+//                     else if (a[3] > b[3]) return -1
+//                     else {
+//                         if (a[0] > b[0]) return 1
+//                         else if (a[0] < b[0]) return -1
+//                         else return 0
+//                     }
+//                 }
+//             }
+//         });
+
+result = arr
+    .map(v => v.split(" ").map(vv => Number(vv) || vv))
+    .sort((a, b) => 
+        b[1] - a[1] || 
+        a[2] - b[2] || 
+        b[3] - a[3] || 
+        a[0].toString().localeCompare(b[0].toString())
+    );
 
 result.forEach(v => names.push(v[0]));
 console.log(names.join("\n"));
