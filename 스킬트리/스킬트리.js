b16e6d85f2924c88534d84efe412f741bc0a36f5
허승회 <35622185+huhsway@users.noreply.github.com>
2023-12-28 10:59:51 +0900

    Update 스킬트리.js

--- 스킬트리/스킬트리.js
+++ 스킬트리/스킬트리.js
@@ -1,50 +1,52 @@
 // 스킬 트리 문제
 // 출처 : http://pgrblues.egloos.com/m/5317734#n
 
-function dfs(skillTree, costTree, rootIndex, skillPoint) {
-    let sumSp = 0
-    if (skillTree[rootIndex].length === 0) {
-        return costTree[rootIndex] = skillPoint
-    }
+// 답 잘못 됐음
 
-    for (const childIdx of skillTree[rootIndex]) {
-        sumSp += dfs(skillTree, costTree, childIdx, skillPoint)
-    }
-    return costTree[rootIndex] = sumSp
-}
+// function dfs(skillTree, costTree, rootIndex, skillPoint) {
+//     let sumSp = 0
+//     if (skillTree[rootIndex].length === 0) {
+//         return costTree[rootIndex] = skillPoint
+//     }
 
-function solution(total_sp, skills) {
+//     for (const childIdx of skillTree[rootIndex]) {
+//         sumSp += dfs(skillTree, costTree, childIdx, skillPoint)
+//     }
+//     return costTree[rootIndex] = sumSp
+// }
 
-    const answer = []
+// function solution(total_sp, skills) {
 
-    let max = 0
-    for (const skill of skills) {
-        if (skill[1] > max) max = skill[1]
-    }
+//     const answer = []
 
-    const skillTree = Array(max + 1).fill().map(() => []);
+//     let max = 0
+//     for (const skill of skills) {
+//         if (skill[1] > max) max = skill[1]
+//     }
 
-    for (const skill of skills) {
-         skillTree[skill[0]].push(skill[1])
-    }
+//     const skillTree = Array(max + 1).fill().map(() => []);
 
-    let adjCnt = 0
-    for (const subTree of skillTree) {
-        adjCnt += subTree.length
-    }
+//     for (const skill of skills) {
+//          skillTree[skill[0]].push(skill[1])
+//     }
 
-    const sp = Math.floor(total_sp / (max + adjCnt))
+//     let adjCnt = 0
+//     for (const subTree of skillTree) {
+//         adjCnt += subTree.length
+//     }
 
-    const costTree = Array(max + 1)
-    dfs(skillTree, costTree, 1, sp)
+//     const sp = Math.floor(total_sp / (max + adjCnt))
 
-    for (let i = 0; i < costTree.length; i++) {
-        answer.push(costTree[i]);
-    }
+//     const costTree = Array(max + 1)
+//     dfs(skillTree, costTree, 1, sp)
 
-    return answer.filter(it => it !== undefined)
-}
+//     for (let i = 0; i < costTree.length; i++) {
+//         answer.push(costTree[i]);
+//     }
+
+//     return answer.filter(it => it !== undefined)
+// }
 
 
 
-console.log(solution(121, [[1,2],[1,3],[3,6],[3,4],[3,5]]))
+// console.log(solution(121, [[1,2],[1,3],[3,6],[3,4],[3,5]]))
