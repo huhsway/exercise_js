258ce41c369d40e5cbdeb207d79fcffe37ce7ddf
허승회 <35622185+huhsway@users.noreply.github.com>
2025-04-22 22:39:02 +0900

    Two_Sum.js 업데이트

--- LeetCode/Array/Two_Sum.js
+++ LeetCode/Array/Two_Sum.js
@@ -5,7 +5,7 @@
  */
 
 // vals 객체를 만들어 target에서 nums[i]를 뺀 값이 vals에 들어있는걸 확인하는게 핵심
-var twoSum = function twoSum(nums, target) {
+var twoSum = function (nums, target) {
     let vals = {};
   
     for (let i = 0; i < nums.length; i++) {
