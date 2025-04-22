ac72289458097b416f752e851640fd34d73b25c3
허승회 <35622185+huhsway@users.noreply.github.com>
2025-04-22 22:40:01 +0900

    3Sum.js 업데이트

--- LeetCode/Array/3Sum.js
+++ LeetCode/Array/3Sum.js
@@ -4,33 +4,36 @@
  */
 
 // 배열을 순회하면서 하나의 값을 잡고 나머지는 시간을 줄이기 위해 이진탐색으로 접근
-var threeSum = function (nums) {
+var threeSum = function(nums) {
   const answer = [];
   const N = nums.length;
 
   nums.sort((a, b) => a - b);
 
-  for (let standard = 0; standard < N - 2; standard++) {
-    let left = standard + 1;
+  for (let i = 0; i < N - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue; // 중복 기준값 건너뜀
+
+    let left = i + 1;
     let right = N - 1;
 
-    if (nums[standard] === nums[standard - 1]) continue;
-
     while (left < right) {
-      const total = nums[standard] + nums[left] + nums[right];
-      if (total === 0) {
-        answer.push([nums[standard], nums[left], nums[right]]);
-        while (left < right && nums[left] === nums[left + 1]) left++;
-        while (left < right && nums[right] === nums[right - 1]) right--;
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        answer.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left] === nums[left + 1]) left++;   // 중복 제거
+        while (left < right && nums[right] === nums[right - 1]) right--; // 중복 제거
 
         left++;
         right--;
-      } else if (total > 0) {
-        right--;
-      } else {
+      } else if (sum < 0) {
         left++;
+      } else {
+        right--;
       }
     }
   }
+
   return answer;
 };
