5dab149eb7b28f210707a55b1556fe2ea95caf78
허승회 <35622185+huhsway@users.noreply.github.com>
2025-04-23 00:11:41 +0900

    Longest_Palindromic_Substring.js 업데이트

--- LeetCode/String/Longest_Palindromic_Substring.js
+++ LeetCode/String/Longest_Palindromic_Substring.js
@@ -6,39 +6,55 @@
  * @param {string} s
  * @return {string}
  */
-function is_Palindrome(str1) {
-    var rev = str1.split("").reverse().join("");
-    return str1 == rev;
-}
-var longestPalindrome = function(s) {
-    let n = s.length; // calculating size of string
-    var isPalindrome = is_Palindrome(s);
-    if(isPalindrome) return s;
-    let maxLength = 1,start=0;
-    let low, high;
-    for (let i = 0; i < n; i++) {
-        low = i - 1;
-        high = i + 1;
-        while ( high < n && s[high] == s[i]) //increment 'high'                                
-            high++;
-      
-        while ( low >= 0 && s[low] == s[i]) // decrement 'low'                
-            low--;
-      
-        while (low >= 0 && high < n && s[low] == s[high]){
-            low--;
-            high++;
-        }
-  
-        let length = high - low - 1;
-        if (maxLength < length) {
-            maxLength = length;
-            start=low+1;
-        }
-    }
-      
-    // document.write("Longest palindrome substring is: ");
-    // document.write();
-    return s.substring(start,maxLength+start);
+function isPalindrome(str) {
+    const reversedStr = str.split("").reverse().join("");
+    return str === reversedStr;
 }
 
+var longestPalindrome = function(s) {
+    const n = s.length;
+    if (n < 2 || isPalindrome(s)) {
+        return s;
+    }
+
+    let maxLength = 1;
+    let start = 0;
+
+    // 홀수 길이 Palindrome 탐색 (중심 문자 i)
+    for (let i = 0; i < n; i++) {
+        let low = i - 1;
+        let high = i + 1;
+        while (low >= 0 && high < n && s[low] === s[high]) {
+            if (high - low + 1 > maxLength) {
+                maxLength = high - low + 1;
+                start = low;
+            }
+            low--;
+            high++;
+        }
+    }
+
+    // 짝수 길이 Palindrome 탐색 (중심 문자 i, i+1)
+    for (let i = 0; i < n - 1; i++) {
+        let low = i;
+        let high = i + 1;
+        while (low >= 0 && high < n && s[low] === s[high]) {
+            if (high - low + 1 > maxLength) {
+                maxLength = high - low + 1;
+                start = low;
+            }
+            low--;
+            high++;
+        }
+    }
+
+    return s.substring(start, start + maxLength);
+};
+
+// 테스트 코드 (선택 사항)
+console.log(longestPalindrome("babad"));   // "bab" 또는 "aba"
+console.log(longestPalindrome("cbbd"));    // "bb"
+console.log(longestPalindrome("a"));       // "a"
+console.log(longestPalindrome("ac"));      // "a"
+console.log(longestPalindrome("racecar")); // "racecar"
+console.log(longestPalindrome("abba"));    // "abba"
