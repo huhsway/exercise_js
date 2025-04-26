5fa33ba6e382edda859d42d9c425db3a91a10079
huhsway <seunghoe9542@naver.com>
2025-04-26 16:28:31 +0900

    20250426_2

--- LeetCode/String/C_Minimum_Window_Substring.js
+++ LeetCode/String/C_Minimum_Window_Substring.js
@@ -7,40 +7,45 @@
 // https://leetcode.com/problems/minimum-window-substring/ 
 
 var minWindow = function(s, t) {
-    const sLen = s.length;
-    const tLen = t.length;
-    let minValue = Number.MAX_SAFE_INTEGER;
-    let count = tLen;
-    let start = 0;
-    let left = 0;
-    let right = 0;
-  
-    const map = t.split('').reduce((acc, cur) => {
-        if(acc[cur]) acc[cur] += 1;
-        else acc[cur] = 1;
-        return acc;
-    },{})
-  
-    while (right < sLen) {
-      if (map[s[right]] !== undefined) {
-        if (map[s[right]] > 0) count--;
-        map[s[right]]--;
-      }
-      right++;
-  
-      while (count === 0) {
-        if (right - left < minValue) {
-          minValue = right - left;
-          start = left;
-        }
-  
-        if (map[s[left]] !== undefined) {
-          if (map[s[left]] === 0) count++;
-          map[s[left]]++;
-        }
-        left++;
-      }
+  const sLen = s.length;
+  const tLen = t.length;
+  let minValue = Number.MAX_SAFE_INTEGER;
+  let count = tLen;
+  let start = 0;
+  let left = 0;
+  let right = 0;
+
+  const tList = t.split('').reduce((acc, cur) => {
+      if(acc[cur]) acc[cur] += 1;
+      else acc[cur] = 1;
+      return acc;
+  },{})
+
+  while (right < sLen) {
+    if (tList[s[right]] !== undefined) {
+      if (tList[s[right]] > 0) count--;
+      tList[s[right]]--;
     }
-  
-    return minValue === Number.MAX_SAFE_INTEGER ? '' : s.substr(start, minValue);
-  };
+    right++;
+
+    while (count === 0) {
+      if (right - left < minValue) {
+        minValue = right - left;
+        start = left;
+      }
+
+      if (tList[s[left]] !== undefined) {
+        if (tList[s[left]] === 0) count++;
+        tList[s[left]]++;
+      }
+      left++;
+    }
+  }
+
+  return minValue === Number.MAX_SAFE_INTEGER ? '' : s.substr(start, minValue);
+};
+
+const s = "ADOBECODEBANC";
+const t = "ABC";
+
+minWindow(s, t)
