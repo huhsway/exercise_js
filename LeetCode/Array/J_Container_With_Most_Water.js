682b2f2324ec30ae468be79d87c535aeeb9d2599
huhsway <seunghoe9542@naver.com>
2025-05-06 18:20:27 +0900

    20250506_4

--- LeetCode/Array/J_Container_With_Most_Water.js
+++ LeetCode/Array/J_Container_With_Most_Water.js
@@ -7,7 +7,7 @@
 
 // 기준값을 놓고 범위를 줄여서 최댓값을 찾자
 var maxArea = function(height) {
-    let max = 0;
+    let maxValue = 0;
     let start = 0;
     let end = height.length - 1;
     
@@ -16,7 +16,7 @@ var maxArea = function(height) {
         const gap = (end - start) * standard;
               
         height[end] > height[start] ? start++ : end--;
-        max = Math.max(max, gap);
+        maxValue = Math.max(maxValue, gap);
     }
-    return max;
+    return maxValue;
 };
