4d2d896c3d2c91aaacf718c27fe39e6033da27df
huhsway <seunghoe9542@naver.com>
2025-05-06 14:36:04 +0900

    20250506

--- LeetCode/Array/B_Best_Time_to_Buy_and_Sell_Stock.js
+++ LeetCode/Array/B_Best_Time_to_Buy_and_Sell_Stock.js
@@ -8,14 +8,14 @@
 // 내가 산거보다 싼게 있다면 사서 비교하는게 핵심
 var maxProfit = function(prices) {
     
-    let buy = prices[0], max_profit = 0, length = prices.length;
+    let buy = prices[0], maxValue = 0, length = prices.length;
     for (let i = 1; i < length; i++) {
         if (buy > prices[i]) {
             buy = prices[i];
         }
-        else if (prices[i] - buy > max_profit) {
-            max_profit = prices[i] - buy;
+        else if (prices[i] - buy > maxValue) {
+            maxValue = prices[i] - buy;
         }
     }
-    return max_profit;
+    return maxValue;
 };
