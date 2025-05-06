8358125032643b907219cc0340672b077f75e305
huhsway <seunghoe9542@naver.com>
2025-05-06 17:00:43 +0900

    20250506_3

--- LeetCode/Array/D_Product_of_Array_Except_Self.js
+++ LeetCode/Array/D_Product_of_Array_Except_Self.js
@@ -8,16 +8,15 @@
 // 왼쪽에서 곱해 나간 다음에 끝에서 부터 다시 밑으로 원래 배열의 누적을 곱해주자.
 var productExceptSelf = function(nums) {
 
-    const length = nums.length;
-    const arr = Array(length);
+    const arr = new Array(nums.length);
 
     arr[0] = 1;
-    for (let i = 1; i < length; i++) {
+    for (let i = 1; i < nums.length; i++) {
         arr[i] = arr[i-1] * nums[i-1];
     }
     
     let acc = 1;
-    for (let i = length - 2; i >= 0; i--) {
+    for (let i = nums.length - 2; i >= 0; i--) {
         acc *= nums[i+1];
         arr[i] *= acc;
     }
@@ -26,4 +25,4 @@ var productExceptSelf = function(nums) {
 
 };
 
-productExceptSelf([1, 2, 3, 4])
+console.log(productExceptSelf([1, 2, 3, 4]));
--- LeetCode/Array/I_3Sum.js
+++ LeetCode/Array/I_3Sum.js
@@ -8,15 +8,14 @@
 // 배열을 순회하면서 하나의 값을 잡고 나머지는 시간을 줄이기 위해 이진탐색으로 접근
 var threeSum = function(nums) {
   const answer = [];
-  const N = nums.length;
 
   nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < N - 2; i++) {
+  for (let i = 0; i < nums.length - 2; i++) {
     if (i > 0 && nums[i] === nums[i - 1]) continue; // 중복 기준값 건너뜀
 
     let left = i + 1;
-    let right = N - 1;
+    let right = nums.length - 1;
 
     while (left < right) {
       const sum = nums[i] + nums[left] + nums[right];
