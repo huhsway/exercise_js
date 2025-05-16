019442417757d07314451394acd50cdd56c3da62
허승회 <35622185+huhsway@users.noreply.github.com>
2025-05-16 17:04:57 +0900

    Update H_Longest_Palindromic_Substring.js

--- LeetCode/String/H_Longest_Palindromic_Substring.js
+++ LeetCode/String/H_Longest_Palindromic_Substring.js
@@ -54,6 +54,40 @@ var longestPalindrome = function(s) {
     return s.substring(start, start + maxLength);
 };
 
+// var longestPalindrome = function(s) {
+//     const n = s.length;
+//     if (n < 2) return s;
+
+//     let start = 0, maxLength = 1;
+
+//     const isPalindrome = (left, right) => {
+//         while (left >= 0 && right < n && s[left] === s[right]) {
+//             left--;
+//             right++;
+//         }
+//         return [left + 1, right - left - 1];  // 시작 위치와 길이 반환
+//     };
+
+//     for (let i = 0; i < n; i++) {
+//         // 홀수 길이 팰린드롬 (i가 중심)
+//         let [oddStart, oddLength] = isPalindrome(i, i);
+//         if (oddLength > maxLength) {
+//             start = oddStart;
+//             maxLength = oddLength;
+//         }
+
+//         // 짝수 길이 팰린드롬 (i와 i+1이 중심)
+//         let [evenStart, evenLength] = isPalindrome(i, i + 1);
+//         if (evenLength > maxLength) {
+//             start = evenStart;
+//             maxLength = evenLength;
+//         }
+//     }
+
+//     return s.substring(start, start + maxLength);
+// };
+
+
 // 테스트 코드 (선택 사항)
 console.log(longestPalindrome("babad"));   // "bab" 또는 "aba"
 console.log(longestPalindrome("cbbd"));    // "bb"
