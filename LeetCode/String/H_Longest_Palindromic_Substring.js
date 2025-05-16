e1153e8663ceaf4e96c4f9a7ee23f9c4c5259b8a
허승회 <35622185+huhsway@users.noreply.github.com>
2025-05-16 17:16:12 +0900

    Update H_Longest_Palindromic_Substring.js

--- LeetCode/String/H_Longest_Palindromic_Substring.js
+++ LeetCode/String/H_Longest_Palindromic_Substring.js
@@ -9,83 +9,38 @@
 
 // https://leetcode.com/problems/longest-palindromic-substring/
 
-function isPalindrome(str) {
-    const reversedStr = str.split("").reverse().join("");
-    return str === reversedStr;
-}
-
 var longestPalindrome = function(s) {
     const n = s.length;
-    if (n < 2 || isPalindrome(s)) {
-        return s;
-    }
+    if (n < 2) return s;
 
-    let maxLength = 1;
-    let start = 0;
+    let start = 0, maxLength = 1;
 
-    // 홀수 길이 Palindrome 탐색 (중심 문자 i)
-    for (let i = 0; i < n; i++) {
-        let low = i - 1;
-        let high = i + 1;
-        while (low >= 0 && high < n && s[low] === s[high]) {
-            if (high - low + 1 > maxLength) {
-                maxLength = high - low + 1;
-                start = low;
-            }
-            low--;
-            high++;
+    const isPalindrome = (left, right) => {
+        while (left >= 0 && right < n && s[left] === s[right]) {
+            left--;
+            right++;
         }
-    }
+        return [left + 1, right - left - 1];  // 시작 위치와 길이 반환
+    };
 
-    // 짝수 길이 Palindrome 탐색 (중심 문자 i, i+1)
-    for (let i = 0; i < n - 1; i++) {
-        let low = i;
-        let high = i + 1;
-        while (low >= 0 && high < n && s[low] === s[high]) {
-            if (high - low + 1 > maxLength) {
-                maxLength = high - low + 1;
-                start = low;
-            }
-            low--;
-            high++;
+    for (let i = 0; i < n; i++) {
+        // 홀수 길이 팰린드롬 (i가 중심)
+        let [oddStart, oddLength] = isPalindrome(i, i);
+        if (oddLength > maxLength) {
+            start = oddStart;
+            maxLength = oddLength;
+        }
+
+        // 짝수 길이 팰린드롬 (i와 i+1이 중심)
+        let [evenStart, evenLength] = isPalindrome(i, i + 1);
+        if (evenLength > maxLength) {
+            start = evenStart;
+            maxLength = evenLength;
         }
     }
 
     return s.substring(start, start + maxLength);
 };
-
-// var longestPalindrome = function(s) {
-//     const n = s.length;
-//     if (n < 2) return s;
-
-//     let start = 0, maxLength = 1;
-
-//     const isPalindrome = (left, right) => {
-//         while (left >= 0 && right < n && s[left] === s[right]) {
-//             left--;
-//             right++;
-//         }
-//         return [left + 1, right - left - 1];  // 시작 위치와 길이 반환
-//     };
-
-//     for (let i = 0; i < n; i++) {
-//         // 홀수 길이 팰린드롬 (i가 중심)
-//         let [oddStart, oddLength] = isPalindrome(i, i);
-//         if (oddLength > maxLength) {
-//             start = oddStart;
-//             maxLength = oddLength;
-//         }
-
-//         // 짝수 길이 팰린드롬 (i와 i+1이 중심)
-//         let [evenStart, evenLength] = isPalindrome(i, i + 1);
-//         if (evenLength > maxLength) {
-//             start = evenStart;
-//             maxLength = evenLength;
-//         }
-//     }
-
-//     return s.substring(start, start + maxLength);
-// };
 
 
 // 테스트 코드 (선택 사항)
