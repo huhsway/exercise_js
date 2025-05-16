04e940ca47082d7221943929bde685c865ab6cd9
허승회 <35622185+huhsway@users.noreply.github.com>
2025-05-16 17:15:27 +0900

    Update A_Longest_Substring_Without_Repeating_Characters.js

--- LeetCode/String/A_Longest_Substring_Without_Repeating_Characters.js
+++ LeetCode/String/A_Longest_Substring_Without_Repeating_Characters.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/ 
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 var lengthOfLongestSubstring = function(s) {
     if (!s) return 0;
-    
+
     let check = '';
     const result = [];
-    
+
     for (let i = 0; i < s.length; i++) {
         if (check.includes(s[i])) {
-            check = check.slice(check.indexOf(s[i]) + 1);
+            check = check.substring(check.indexOf(s[i]) + 1);
         }
-      
+
         check += s[i];
         result.push(check.length);
     }
-    
+
     return Math.max(...result);
 };
