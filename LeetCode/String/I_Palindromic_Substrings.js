5c8a1bff0b22f74b18fd9ad018ed89c101253613
huhsway <seunghoe9542@naver.com>
2025-05-27 02:03:35 +0900

    20250527

--- LeetCode/String/I_Palindromic_Substrings.js
+++ LeetCode/String/I_Palindromic_Substrings.js
@@ -5,23 +5,21 @@
 
 // https://leetcode.com/problems/palindromic-substrings/description/
 
-const countSubstrings = (s) => {
+var countSubstrings = (s) => {
     let count = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        count += getPalCount(s, i, i);    // odd length strings
-        count += getPalCount(s, i, i+1);  // even length strings
-    }
 
-    return count
-};
-
-const getPalCount = (s, l, r) => {
-    count = 0;
+    const getPalCount = (l, r) => {
         while (l >= 0 && r < s.length && s[l] === s[r]) {
             count++;
             l--;
             r++;
         }
-    return count;
-}
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        getPalCount(i, i);    // odd length strings
+        getPalCount(i, i+1);  // even length strings
+    }
+
+    return count
+};
